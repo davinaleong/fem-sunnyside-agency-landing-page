3357341e2530ba2d4b7b7c73a018fb24ce676728
Davina Leong <davina.leong.sy@gmail.com>
2022-08-15 17:35:20 +0800

    Able to toggle menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-console.log(`Script loaded`)
+const primaryHeader = document.querySelector(`[data-primary-header]`)
+
+const primaryHeaderToggleButton = document.querySelector(
+  `[data-primary-navigation-toggle]`
+)
+
+primaryHeaderToggleButton.addEventListener(`click`, () => {
+  primaryHeader.toggleAttribute(`data-open`)
+})
